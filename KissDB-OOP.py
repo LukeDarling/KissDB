26e5322281fbb0c57fe705737ef761fe230c8f39
Luke Darling <luke@lukedarling.dev>
2021-02-11 22:22:07 -0500

    Began conversion to OOP singleton structure

---
+++ KissDB-OOP.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# KissDB  written by Luke Darling.
+# All rights reserved.
+
+# Imports
+import os, time, datetime, uuid, signal, shutil, fcntl, threading, socket
+import json, yaml
+
+class KissDBDaemon:
+
+    instance = KissDBDaemon()
+
+    def __init__():
+        pass
+
+    def __del__():
+        pass
